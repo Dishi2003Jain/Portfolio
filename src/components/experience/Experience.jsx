a4bf60f3d491731e67d1e04243d624f920063cb9
Dishi <dishinayak223@gmail.com>
2023-01-07 22:30:51 +0530

    Updateexperience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -9,28 +9,28 @@ const Experience = () => {
     <ul>
       <li>
         <div class="timeline-content">
-          <h3 class="date">20th may, 2010</h3>
+          <h3 class="date">May 2022-June 2022</h3>
           <h3>MENTEE-Microsoft engage</h3>
           <p>Got selected for the Microsoft engage 2021 program where I had develop a personal project based on the given problem statement.</p>
         </div>
       </li>
       <li>
         <div class="timeline-content">
-          <h3 class="date">20th may, 2010</h3>
+          <h3 class="date">March 2022-June 2022</h3>
           <h3>Reeya community</h3>
           <p>Worked as a full stack developer and built the website for the community and also got the opportunity to lead the team.</p>
         </div>
       </li>
       <li>
         <div class="timeline-content">
-          <h3 class="date">20th may, 2010</h3>
+          <h3 class="date">October 2021-Present</h3>
           <h3>Codame Club,IIIT Bhopal</h3>
           <p>Contributed in hosting web development events and in developing the official website for the codame club. </p>
         </div>
       </li>
       <li>
         <div class="timeline-content">
-          <h3 class="date">Feb 2021 - June 2021</h3>
+          <h3 class="date">Feb 2021-June 2021</h3>
           <h3>IIITian's network</h3>
           <p>Collaborated with the students from the various IIITs and worked on the the projects of the mutual interests.</p>
         </div>
